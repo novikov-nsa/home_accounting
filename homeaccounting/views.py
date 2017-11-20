e52b41c3fa5af7b89982552b0641c59f0d91ac1c
snovikov <novikov.nsa@live.com>
2017-11-21 00:59:23 +0300

    Группировка по датам с выводом операций по каждой дате.

--- homeaccounting/views.py
+++ homeaccounting/views.py
@@ -12,18 +12,11 @@ class MainView(TemplateView):
         #latest_oper = Operations.objects.order_by('dateOper')
         latest_oper = list(Operations.objects.all().values('dateOper') \
             .annotate(sum_on_date=Sum('sumOper')).order_by('-dateOper'))
-        for item_date in latest_oper:
-            print(item_date['dateOper'])
-            print('\n')
-            list_oper_to_date = list(Operations.objects.filter(dateOper=item_date['dateOper'])\
-                                                          .values('nameOper', 'accDtOper__accountCode',\
-                                                                  'accDtOper__accountName', 'accKtOper__accountCode',\
-                                                                  'accKtOper__accountName', 'sumOper'))
-            print(list_oper_to_date)
-            #latest_oper[item_date] =list(latest_oper[item_date], **list_oper_to_date)
 
+        list_oper_to_date = list(Operations.objects.all()\
+                                .values('dateOper', 'nameOper', 'accDtOper__accountCode',\
+                                        'accDtOper__accountName', 'accKtOper__accountCode',\
+                                        'accKtOper__accountName', 'sumOper'))
 
-        print(latest_oper)
-
-        context = {'latest_oper': latest_oper}
+        context = {'latest_oper': latest_oper, 'list_oper_to_date':list_oper_to_date}
         return render(request, template_name, context)
