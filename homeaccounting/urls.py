9a2e2640f2508d2e3099d483654948d1bfadd625
snovikov <novikov.nsa@live.com>
2017-11-26 21:40:02 +0300

    Попытка прикрутить аутентиыфикацию (не у дачная)

--- homeaccounting/urls.py
+++ homeaccounting/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from django.conf.urls import url, include
+from django.contrib.auth.views import login
 
 from .views import MainView, NewDocFormView
 
@@ -7,5 +9,6 @@ from .views import MainView, NewDocFormView
 urlpatterns = [
     url(r'^$', MainView.as_view(), name='main-view'),
     url(r'^new/', NewDocFormView.as_view()),
+    url('^login/$', login, {'redirect_field_name': 'main-view'}),
 
 ]
--- homeaccounting/views.py
+++ homeaccounting/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from .models import Accounts, Orginfo, Operations
 from django.db.models import Sum
-from django.views.generic import TemplateView, FormView, CreateView
+from django.views.generic import TemplateView, FormView, CreateView, View
 import datetime
 from .forms import NewDocForm
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
+
+
 
 # Create your views here.
-class MainView(TemplateView):
+class MainView(AccessMixin, View):
+    #login_url = ''
+    redirect_field_name = 'main-view'
+
+
 
     def get(self, request, *args, **kwargs):
+
         template_name = 'main.html'
         #latest_oper = Operations.objects.order_by('dateOper')
         latest_oper = list(Operations.objects.all().values('dateOper') \
@@ -19,10 +27,16 @@ class MainView(TemplateView):
                                         'accDtOper__accountName', 'accKtOper__accountCode',\
                                         'accKtOper__accountName', 'sumOper'))
 
-        context = {'latest_oper': latest_oper, 'list_oper_to_date':list_oper_to_date}
+        url = AccessMixin.get_login_url(self)
+        context = {'latest_oper': latest_oper, 'list_oper_to_date':list_oper_to_date, 'url':url}
+
+        #username = request.POST['username']
+        #password = request.POST['password']
+        #user = authenticate(username=username, password=password)
+        #print(user)
         return render(request, template_name, context)
 
-class NewDocFormView(CreateView):
+class NewDocFormView(LoginRequiredMixin, CreateView):
     template_name = 'new_doc.html'
     #form_class = NewDocForm
     model = Operations
